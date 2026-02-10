efba46e3087555a7f75c56a872c66a61d0ab9413
ellah16-hub <ellahkevin16@gmail.com>
2026-02-10 15:31:40 +0100

    Update bst.c

--- src/bst.c
+++ src/bst.c
@@ -87,6 +87,31 @@ NodePtr delNode(NodePtr root, int x) {
     }
     return root;
 }
+void inorder(NodePtr root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+// Preorder: Root -> Left -> Right
+void preorder(NodePtr root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+// Postorder: Left -> Right -> Root
+void postorder(NodePtr root) {
+    if (root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
 
 
 int main() {
